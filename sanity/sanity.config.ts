bea9dfb2f34cda9d77f02c97a2946304d1b511ca
ihack168 <ihack168@gmail.com>
2026-05-08 18:17:53 +0000

    s

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,18 +1,20 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import {schemaTypes} from './schemaTypes' // 確保這行指向包含 index.ts 的資料夾
 
 export default defineConfig({
   name: 'default',
   title: 'line88-blog',
 
+  // 這是你 Lockhead Hex 專案的專屬 ID
   projectId: 't0di9pwy',
   dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
 
   schema: {
+    // 這裡會讀取你 index.ts 裡面的 [post, author, category, blockContent]
     types: schemaTypes,
   },
 })
