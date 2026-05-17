48be21c6a546e325a4bf9d45ce6eae059f9812cd
ihack168 <ihack168@gmail.com>
2026-05-17 10:26:21 +0800

    s

--- .github/scripts/auto-post.mjs
+++ .github/scripts/auto-post.mjs
@@ -9,7 +9,7 @@ const SHEET_NAME = 'line88';
 
 // Google Apps Script 網址
 const GOOGLE_SCRIPT_BASE_URL =
-  'https://script.google.com/macros/s/AKfycbwFpZDhMveHhdOYdDkh02JpWk28jUCBqikyM-Urg_6Uw2jTH7d8ZluKxinKTWh5_20N/exec?sheet=line88;
+  'https://script.google.com/macros/s/AKfycbwFpZDhMveHhdOYdDkh02JpWk28jUCBqikyM-Urg_6Uw2jTH7d8ZluKxinKTWh5_20N/exec?sheet=line88';
 
 const GOOGLE_SCRIPT_URL =
   `${GOOGLE_SCRIPT_BASE_URL}?sheet=${encodeURIComponent(SHEET_NAME)}`;
