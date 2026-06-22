b649495bb990639bd3b2f333bcc107e61e758d25
ihack168 <ihack168@gmail.com>
2026-06-22 20:51:13 +0800

    s

--- components/share-bar.tsx
+++ components/share-bar.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
-export function FloatingBar() {
+export function ShareBar() {
   const [url, setUrl] = useState<string>("");
   const [copied, setCopied] = useState<boolean>(false);
 
