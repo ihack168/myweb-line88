8f3abf117cb8b91f4d09fe5d588fc0129d5d2a8e
ihack168 <ihack168@gmail.com>
2026-08-04 17:23:07 +0800

    ㄋ

--- app/api/socialmedia/route.ts
+++ app/api/socialmedia/route.ts
@@ -1,3 +1,15 @@
+export async function POST() {
+  return Response.json(
+    {
+      success: false,
+      error: "This API has been disabled."
+    },
+    {
+      status: 403
+    }
+  );
+}
+
 import { NextResponse } from "next/server";
 import { Converter } from "opencc-js";
 import { Redis } from "@upstash/redis";
